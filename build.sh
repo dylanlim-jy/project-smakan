91c0ff91821a8aa2934c94ff2faa61f900c55534
Dylan Lim <dylan@Dylans-MacBook-Air-M1.local>
2023-03-23 01:38:12 +0800

    Edit render build filepath

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ set -o errexit
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 
-python smakan/manage.py collectstatic --no-input
-python smakan/manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py migrate

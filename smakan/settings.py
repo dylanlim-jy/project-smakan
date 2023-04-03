ffa34bdaa89e00b5535dda32d9fad8d979449d6c
Dylan Lim <dylan@Dylans-MacBook-Air-M1.local>
2023-04-03 16:11:32 +0800

    Change staticfiles storage from WhiteNoise to default Django storage

--- smakan/settings.py
+++ smakan/settings.py
@@ -155,7 +155,8 @@ INTERNAL_IPS = [
 # if not DEBUG:    
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
